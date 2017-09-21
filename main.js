0eb105bc6eb2d93e137b25f959f4bb90046bbc3e
nastya <lebedeanastasija@yandex.by>
2017-09-22 00:42:56 +0300

    Add numbers generation

--- main.js
+++ main.js
@@ -13,16 +13,23 @@ let svgContainer,
     },
     startPoint = {
         x: svgMargin.left,
-        y: Math.round((svgMargin.top + svgHeight)/2)
+        y: svgMargin.top + svgHeight
     },
     xAxisLength = 750,
     yAxisLength = 450,
-    scale = 20;
+    scale = 300;
+
+let a = 11;
+let m = 5;
+let Ro = 1;
+let n = 100000;
 
 
 function init() {
     drawSvgContainer(svgWidth, svgHeight, svgMargin);
     drawAxes(xAxisLength, yAxisLength, startPoint, scale);
+    let numbers = generateRandomNumbers(a, m, Ro, n);
+    drawPoints(numbers, colors[12]);
 }
 
 function task1() {
@@ -33,6 +40,23 @@ function task1() {
 function task2() {
     clearSvgContainer(svgContainer);
     drawAxes(xAxisLength, yAxisLength, startPoint, scale);
+}
+
+
+function generateRandomNumbers(a, m, Ro, n) {
+    let result = [];
+    //result.push(Ro / m);
+    let prevRemnant = Ro;
+    for(let i = 1; i < n; i++) {
+        let remnant = getNextRandomRemnant(a, m, prevRemnant);
+        result.push({x: i, y: remnant / m});
+        prevRemnant = remnant;
+    };
+    return result;
+}
+
+function getNextRandomRemnant(a, m, R) {
+    return (a * R) % m;
 }
 
 
@@ -48,15 +72,15 @@ function clearSvgContainer(svg) {
 }
 
 function drawAxes(xLength, yLength, startPoint, scale) {
-    let xScale = d3.scaleLinear().domain([0, xLength]).range([0, xLength]);
-    let yScale = d3.scaleLinear().domain([-1 * yAxisLength / scale / 2, yAxisLength / scale / 2]).range([yLength, 0]);
+    let xScale = d3.scaleLinear().domain([0, xLength * scale * 5]).range([0, xLength]);
+    let yScale = d3.scaleLinear().domain([0, yAxisLength / scale]).range([yLength, 0]);
     let xAxis = d3.axisBottom().scale(xScale);
     let yAxis = d3.axisLeft().scale(yScale);
 
     svgContainer
     .append("g")
     .attr('class', 'axis')
-    .attr('transform', 'translate(' + startPoint.x + ',' + Math.round(svgMargin.top * 1.75) + ')')
+    .attr('transform', 'translate(' + startPoint.x + ',' + Math.round(svgMargin.top * 3.5) + ')')
     .call(yAxis);
 
     svgContainer
